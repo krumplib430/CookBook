4c622785e66c16beec2d9c91187370ae5bdb50b7
Bence Farkas <farkas.bence@gmail.com>
2017-11-18 20:31:55 +0100

    Added stubs for components and basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,30 @@ import {NgModule} from '@angular/core';
 import {AngularFireModule} from 'angularfire2';
 import {AngularFireDatabaseModule} from 'angularfire2/database';
 import {AngularFireAuthModule} from 'angularfire2/auth';
-
 import {MatComponentsModule} from './mat-components.module';
 
 import {environment} from '../environments/environment';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {RecipeListComponent} from './recipe-list/recipe-list.component';
+import {HomeComponent} from './home/home.component';
 import {MainNavbarComponent} from './main-navbar/main-navbar.component';
-
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {RecipeComponent} from './recipe/recipe.component';
+import {RecipeEditComponent} from './recipe-edit/recipe-edit.component';
+import {RecipeListComponent} from './recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    MainNavbarComponent,
+    LoginComponent,
+    RegisterComponent,
     RecipeListComponent,
-    MainNavbarComponent
+    RecipeComponent,
+    RecipeEditComponent
   ],
   imports: [
     BrowserModule,
