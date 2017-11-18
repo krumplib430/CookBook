ddb69f76d43335b6da78d7f8f660ef67d91d88d4
Bence Farkas <farkas.bence@gmail.com>
2017-11-18 10:47:20 +0100

    Added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NgModule} from '@angular/core';
 import {AngularFireModule} from 'angularfire2';
 import {AngularFireDatabaseModule} from 'angularfire2/database';
 import {AngularFireAuthModule} from 'angularfire2/auth';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+
 import {environment} from '../environments/environment';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {RecipeListComponent} from './recipe-list/recipe-list.component';
-import { MainNavbarComponent } from './main-navbar/main-navbar.component';
+import {MainNavbarComponent} from './main-navbar/main-navbar.component';
 
 
 @NgModule({
@@ -19,10 +22,13 @@ import { MainNavbarComponent } from './main-navbar/main-navbar.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    MatCheckboxModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
