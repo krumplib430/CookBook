c772fb73698e196c778797cd3b61b20a1d20d360
Bence Farkas <farkas.bence@gmail.com>
2017-11-25 20:48:37 +0100

    Added auth module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,1 +1,24 @@
+import {ModuleWithProviders, NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ReactiveFormsModule} from '@angular/forms';
 
+import {LoginPageComponent} from './containers/login-page.component';
+import {LoginFormComponent} from './components/login-form.component';
+
+export const COMPONENTS = [LoginPageComponent, LoginFormComponent];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  declarations: COMPONENTS,
+  exports: COMPONENTS
+})
+export class AuthModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: AuthModule
+    };
+  }
+}
