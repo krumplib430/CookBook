9afe78c81033cd0eeb14a2b48e22090c9b812208
Bence Farkas <Bence.Farkas@betssongroup.com>
2017-11-28 10:41:49 +0100

    Fixed auth guard

--- src/app/features/auth/services/auth-guard.ts
+++ src/app/features/auth/services/auth-guard.ts
@@ -2,21 +2,22 @@ import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from '@angular/router';
 import {Observable} from 'rxjs/Observable';
 import {Store} from '@ngrx/store';
+import {AngularFireAuth} from 'angularfire2/auth';
 import 'rxjs/add/operator/take';
 import 'rxjs/add/operator/map';
 import * as authState from '../state/auth';
-import * as authSelectors from '../selectors/auth';
 import * as authActions from '../actions/auth';
+
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private store: Store<authState.State>) {
+  constructor(private store: Store<authState.State>, private afAuth: AngularFireAuth) {
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    return this.store.select(authSelectors.getAuthLoggedIn)
-      .map(loggedIn => {
-        if (!loggedIn) {
+    return this.afAuth.authState
+      .map(state => {
+        if (!state) {
           this.store.dispatch(new authActions.LoginRedirect());
           return false;
         }
